37a2d307b5b8d41edb2cc9f77b12e34ca11ca039
rhenzarg <rhenzarg@gmail.com>
2026-03-04 19:16:04 +0700

    Praktikum3_Percobaan3_Pertanyaan4

--- Praktikum03/MatakuliahDemo28.java
+++ Praktikum03/MatakuliahDemo28.java
@@ -6,12 +6,15 @@ public class MatakuliahDemo28 {
     public static void main(String[] args) {
         Scanner rhenza = new Scanner(System.in);
 
-        Matakuliah28[] arrayMatakuliah28 = new Matakuliah28[3];
+        System.out.print("Masukan jumlah mata kuliah: ");
+        int jumlah = Integer.parseInt(rhenza.nextLine());
+
+        Matakuliah28[] arrayMatakuliah28 = new Matakuliah28[jumlah];
 
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlah; i++) {
             arrayMatakuliah28[i] = new Matakuliah28();
             System.out.println("Masukan Data Mata Kuliah ke-" + (i+1));
             System.out.print("Kode        : ");
@@ -29,7 +32,7 @@ public class MatakuliahDemo28 {
             arrayMatakuliah28[i].tambahData(kode, nama, sks, jumlahJam);
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlah; i++) {
             System.out.println("Data Mata Kuliah ke-" + (i+1));
             arrayMatakuliah28[i].cetakInfo();
             System.out.println("-------------------------------");
