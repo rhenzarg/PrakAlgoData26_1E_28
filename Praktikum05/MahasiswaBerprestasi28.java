e7d8e4248467e4c92729a090122ea0af5415a7a8
rhenzarg <rhenzarg@gmail.com>
2026-04-08 20:02:17 +0700

    P6_Percobaan4_Pertanyaan

--- Praktikum05/MahasiswaBerprestasi28.java
+++ Praktikum05/MahasiswaBerprestasi28.java
@@ -51,10 +51,10 @@ public class MahasiswaBerprestasi28 {
     }
 
     void insertionSort() {
-        for (int i = 1; i < listMhs.length; i++) {
+        for (int i = 1; i < idx; i++) {
             Mahasiswa28 temp = listMhs[i];
             int j = i;
-            while (j> 0 && listMhs[j-1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j-1].ipk < temp.ipk) {
                 listMhs[j] = listMhs[j-1];
                 j--;
             }
